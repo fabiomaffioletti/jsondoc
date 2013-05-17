632de4c710c5a79900e3f3674a33994a023ad3ea
Fabio Maffioletti <fabio.maffioletti@gmail.com>
2013-05-17 20:40:58 +0200

    Added UserController and User object

---
+++ jsondoc-sample-springmvc/src/main/java/org/jsondoc/sample/controller/UserController.java
@@ -0,0 +1,45 @@
+package org.jsondoc.sample.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiError;
+import org.jsondoc.core.annotation.ApiErrors;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiParam;
+import org.jsondoc.core.annotation.ApiResponseObject;
+import org.jsondoc.core.pojo.ApiVerb;
+import org.jsondoc.sample.pojo.User;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Api(name = "User Services", description = "Methods for managing users")
+@Controller
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@ApiMethod(path = "/users/{id}", verb = ApiVerb.GET, description = "Gets a user with the given ID", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@ApiErrors(apierrors = { @ApiError(code = "3000", description = "User not found"), @ApiError(code = "9000", description = "Illegal argument") })
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public @ResponseBody @ApiResponseObject
+	User user(@PathVariable @ApiParam(name = "id", description = "The user's ID", required = true) Integer id) {
+		return new User(id, "jsondoc-user", 30, "M");
+	}
+
+	@ApiMethod(path = "/users/{gender}/{age}", verb = ApiVerb.GET, description = "Gets users with the given gender and age", produces = { MediaType.APPLICATION_JSON_VALUE })
+	@ApiErrors(apierrors = { @ApiError(code = "3000", description = "User not found"), @ApiError(code = "9000", description = "Illegal argument") })
+	@RequestMapping(value = "/{gender}/{age}", method = RequestMethod.GET)
+	public @ResponseBody @ApiResponseObject
+	List<User> userAge(@PathVariable @ApiParam(name = "gender", description = "The user's gender", required = true) String gender, @PathVariable @ApiParam(name = "age", description = "The user's required age", required = true) Integer age) {
+		List<User> users = new ArrayList<User>();
+		users.add(new User(1, "jsondoc-user-1", age, gender));
+		users.add(new User(2, "jsondoc-user-2", age, gender));
+		return users;
+	}
+
+}
---
+++ jsondoc-sample-springmvc/src/main/java/org/jsondoc/sample/pojo/User.java
@@ -0,0 +1,73 @@
+package org.jsondoc.sample.pojo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.jsondoc.core.annotation.ApiObject;
+import org.jsondoc.core.annotation.ApiObjectField;
+
+@ApiObject(name = "user")
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class User {
+
+	@ApiObjectField(description = "The ID of the user")
+	@XmlElement(name = "id")
+	private Integer id;
+	@ApiObjectField(description = "The username of the user")
+	@XmlElement(name = "username")
+	private String username;
+	@ApiObjectField(description = "The age of the user")
+	@XmlElement(name = "age")
+	private Integer age;
+	@ApiObjectField(description = "The gender of the user")
+	@XmlElement(name = "gender")
+	private String gender;
+
+	public User() {
+
+	}
+
+	public User(Integer id, String username, Integer age, String gender) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
