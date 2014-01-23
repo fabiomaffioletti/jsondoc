b8a8c163b2fd77eebd916a8d9f79de22605aff51
nk <nk@dsigned.gr>
2014-01-24 01:18:52 +0200

    Fixes #13: Support for implicit request parameters (also bug in ui where query string is not included in the url)

--- jsondoc-core/src/test/java/org/jsondoc/core/util/controller/Test1Controller.java
+++ jsondoc-core/src/test/java/org/jsondoc/core/util/controller/Test1Controller.java
@@ -1,27 +1,19 @@
 package org.jsondoc.core.util.controller;
 
-import java.util.List;
-
-import org.jsondoc.core.annotation.Api;
-import org.jsondoc.core.annotation.ApiBodyObject;
-import org.jsondoc.core.annotation.ApiError;
-import org.jsondoc.core.annotation.ApiErrors;
-import org.jsondoc.core.annotation.ApiHeader;
-import org.jsondoc.core.annotation.ApiHeaders;
-import org.jsondoc.core.annotation.ApiMethod;
-import org.jsondoc.core.annotation.ApiParam;
-import org.jsondoc.core.annotation.ApiResponseObject;
+import org.jsondoc.core.annotation.*;
 import org.jsondoc.core.pojo.ApiParamType;
 import org.jsondoc.core.pojo.ApiVerb;
 import org.springframework.http.MediaType;
 
+import java.util.List;
+
 @Api(name="Test1Controller", description="My test controller #1")
 public class Test1Controller {
-	
+
 	@ApiMethod(
-			path="/test1", 
-			verb=ApiVerb.GET, 
-			description="test method for controller 1", 
+			path="/test1",
+			verb=ApiVerb.GET,
+			description="test method for controller 1",
 			consumes={MediaType.APPLICATION_JSON_VALUE},
 			produces={MediaType.APPLICATION_JSON_VALUE}
 	)
@@ -32,6 +24,8 @@ public class Test1Controller {
 			@ApiError(code="1000", description="A test error #1"),
 			@ApiError(code="2000", description="A test error #2")
 	})
+    @ApiParams(parameters = {
+            @ApiParam(name = "token", description = "a token", required = false, paramType = ApiParamType.QUERY)})
 	public @ApiResponseObject List<Integer> get(@ApiParam(name="id", description="abc", paramType=ApiParamType.PATH) String id, @ApiParam(name="count", description="xyz", paramType=ApiParamType.PATH) Integer count, @ApiBodyObject String name) {
 		return null;
 	}
