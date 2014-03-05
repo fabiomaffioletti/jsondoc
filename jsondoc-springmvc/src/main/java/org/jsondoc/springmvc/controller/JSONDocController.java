8e8b8ab2e874c879f08e5c56412824b431f36215
massimo-coluzzi <mcoluzzi@ARDA>
2014-03-05 17:18:48 +0100

    Added property to enable cacheing of the produced documentation. This to avoid to rebuild all the documentation at each request.

--- jsondoc-springmvc/src/main/java/org/jsondoc/springmvc/controller/JSONDocController.java
+++ jsondoc-springmvc/src/main/java/org/jsondoc/springmvc/controller/JSONDocController.java
@@ -1,5 +1,6 @@
 package org.jsondoc.springmvc.controller;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.jsondoc.core.pojo.JSONDoc;
@@ -12,26 +13,94 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/jsondoc")
-public class JSONDocController {
+public class JSONDocController
+{
+    
+    private JSONDoc cachedDoc;
+    
 	private String version;
 	private String basePath;
+	private boolean cacheResult;
 	private List<String> packages;
+	
+	
+	public JSONDocController()
+	{
+	    super();
+	    
+	    this.version = "";
+	    this.basePath = "";
+	    this.packages = null;
+	    this.cachedDoc = null;
+	    this.cacheResult = false;
+	    
+	}
+	
+	
+	/* ******************* */
+    /*  INTERFACE METHODS  */
+    /* ******************* */
+	
+	
+	@RequestMapping(method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody JSONDoc getApi()
+	{
+	    
+	    if( cacheResult )
+	    {
+	        if( cachedDoc == null )
+	            cachedDoc = buildApiDoc();
+	        
+	        return cachedDoc;
+	    }
+	        
+        return buildApiDoc();
+        
+    }
+	
+	
+	/* ***************** */
+    /*  PRIVATE METHODS  */
+    /* ***************** */
+	
+	
+	/**
+	 * Checks the parameters and builds the {@link JSONDoc} object.
+	 */
+	private JSONDoc buildApiDoc()
+	{
+	    
+	    if( packages != null )
+	        return SpringMvcDocUtils.getApiDoc( version, basePath, packages );
+	    else
+	        return SpringMvcDocUtils.getApiDoc( version, basePath, new LinkedList<String>() );
+	    
+	}
+	
+	
+	/* ******************* */
+	/*  GETTERS & SETTERS  */
+	/* ******************* */
 
-	public void setVersion(String version) {
+	
+	public void setVersion( String version )
+	{
 		this.version = version;
 	}
 
-	public void setBasePath(String basePath) {
+	public void setBasePath( String basePath )
+	{
 		this.basePath = basePath;
 	}
 
-	public void setPackages(List<String> packages) {
+	public void setPackages( List<String> packages )
+	{
 		this.packages = packages;
 	}
-
-	@RequestMapping(method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody
-	JSONDoc getApi() {
-		return SpringMvcDocUtils.getApiDoc( version, basePath, packages );
-	}
+    
+    public void setCacheResult( boolean cacheResult )
+    {
+        this.cacheResult = cacheResult;
+    }
+	
 }
