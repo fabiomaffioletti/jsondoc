889539b979ed252bc59194300fb2d1e264799199
fmaffioletti <fmaffioletti@ebay.com>
2015-01-22 23:16:46 +0100

    some scripts

---
+++ release-jsondoc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$1" ] 
+    then 
+        echo "Specify release version as first argument"
+	exit 0
+fi
+
+if [ -z "$2" ]
+    then
+        echo "Specify development version as second argument"
+        exit 0
+fi
+
+
+RELEASEVERSION=${1}
+DEVELOPMENTVERSION=${2}
+
+echo "Releasing with version: $RELEASEVERSION"
+
+mvn versions:set -DnewVersion=$RELEASEVERSION
+git add .
+git commit -m "poms for release version $RELEASEVERSION"
+git push origin master
+git tag -a v$RELEASEVERSION -m "tag v$RELEASEVERSION"
+git push --tags
+mvn clean gpg:sign deploy -Dgpg.executable=gpg2 -s ~/.m2/settings.xml
+mvn versions:set -DnewVersion $DEVELOPMENTVERSION
+git add .
+git commit -m "poms for development version $DEVELOPMENTVERSION"
+git push origin master
+
+
+
+
