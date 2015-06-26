bed89bf98ab423377f17e84ace3f1fcebb3f55b7
alessioarsuffi <alessio.arsuffi@gmail.com>
2015-06-26 22:05:47 +0200

    added missing test
    tested cors functionality

--- jsondoc-springmvc/src/test/java/org/jsondoc/springmvc/controller/JSONDocControllerTest.java
+++ jsondoc-springmvc/src/test/java/org/jsondoc/springmvc/controller/JSONDocControllerTest.java
@@ -1,27 +1,7 @@
 package org.jsondoc.springmvc.controller;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration
-//@WebAppConfiguration
 public class JSONDocControllerTest {
 
-//    @Autowired
-//    private WebApplicationContext context;
+	// Nothing to do here. To be tested in the host war.
 
-//    private MockMvc mockMvc;
-
-//	@Test
-//	public void testCorsEnabled_true() throws Exception {
-//		CorsFilter filter = new CorsFilter();
-//		filter.setCorsEnabled(true);
-//
-//        List<String> packages = new ArrayList<String>();
-//        packages.add("foo.bar.package");
-//
-//        standaloneSetup(new JSONDocController("1.0", "http://localhost:8080/", packages))
-//                .addFilter(filter)
-//                .build()
-//                .perform(get("/jsondoc"))
-//                .andExpect(status().isOk());
-//	}
 }
