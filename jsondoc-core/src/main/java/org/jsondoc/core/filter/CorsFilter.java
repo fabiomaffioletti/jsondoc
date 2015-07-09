d253935c06728e3dfe5736346824ce123bde568b
shutter <alessio.arsuffi@gmail.com>
2015-07-09 17:00:00 +0200

    added missing header
    added: Origin, X-Requested-With, Content-Type, Accept, JWT-simulate to: Access-Control-Allow-Headers

--- jsondoc-core/src/main/java/org/jsondoc/core/filter/CorsFilter.java
+++ jsondoc-core/src/main/java/org/jsondoc/core/filter/CorsFilter.java
@@ -40,7 +40,7 @@ public class CorsFilter implements Filter {
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
             response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, JWT-simulate");
 
         }
 
