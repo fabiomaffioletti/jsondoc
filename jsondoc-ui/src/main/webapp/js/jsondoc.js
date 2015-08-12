bad7e62b1aec169c4efcd702917c59506cf14b45
fmaffioletti <fmaffioletti@ebay.com>
2015-08-12 15:37:11 +0200

    fixes #134

---
+++ jsondoc-ui/src/main/webapp/js/jsondoc.js
@@ -0,0 +1,8 @@
+$.urlParam = function(name) {
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	if (results == null) {
+		return null;
+	} else {
+		return results[1] || 0;
+	}
+}
