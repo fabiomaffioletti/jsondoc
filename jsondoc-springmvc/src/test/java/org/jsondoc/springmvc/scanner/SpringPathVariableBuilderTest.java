e4a7e22d1441fed7c35707dc2779c4723aad6af2
fmaffioletti <fmaffioletti@ebay.com>
2015-08-17 11:41:50 +0200

    added assertion on test

--- jsondoc-springmvc/src/test/java/org/jsondoc/springmvc/scanner/SpringPathVariableBuilderTest.java
+++ jsondoc-springmvc/src/test/java/org/jsondoc/springmvc/scanner/SpringPathVariableBuilderTest.java
@@ -37,6 +37,17 @@ public class SpringPathVariableBuilderTest {
 		
 	}
 	
+	@Controller
+	@RequestMapping
+	public class SpringController2 {
+
+		@RequestMapping(value = "/param-one/{id}/{string}")
+		public void paramOne(@ApiPathParam(description = "description for id") @PathVariable Long id, @PathVariable("name") String name) {
+
+		}
+		
+	}
+	
 	@Test
 	public void testPathVariable() {
 		ApiDoc apiDoc = jsondocScanner.getApiDocs(Sets.<Class<?>> newHashSet(SpringController.class), MethodDisplay.URI).iterator().next();
@@ -66,6 +77,27 @@ public class SpringPathVariableBuilderTest {
 				ApiParamDoc test = iterator.next();
 				Assert.assertEquals("", test.getName());
 				Assert.assertEquals("long", test.getJsondocType().getOneLineText());
+			}
+		}
+		
+	}
+	
+	@Test
+	public void testPathVariableWithJSONDoc() {
+		ApiDoc apiDoc = jsondocScanner.getApiDocs(Sets.<Class<?>> newHashSet(SpringController2.class), MethodDisplay.URI).iterator().next();
+		Assert.assertEquals("SpringController2", apiDoc.getName());
+		Assert.assertEquals(1, apiDoc.getMethods().size());
+		for (ApiMethodDoc apiMethodDoc : apiDoc.getMethods()) {
+			if (apiMethodDoc.getPath().contains("/param-one/{id}/{string}")) {
+				Assert.assertEquals(2, apiMethodDoc.getPathparameters().size());
+				Iterator<ApiParamDoc> iterator = apiMethodDoc.getPathparameters().iterator();
+				ApiParamDoc id = iterator.next();
+				Assert.assertEquals("", id.getName());
+				Assert.assertEquals("long", id.getJsondocType().getOneLineText());
+				Assert.assertEquals("description for id", id.getDescription());
+				ApiParamDoc name = iterator.next();
+				Assert.assertEquals("name", name.getName());
+				Assert.assertEquals("string", name.getJsondocType().getOneLineText());
 			}
 		}
 		
